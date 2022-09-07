67106e5c3d98efd01aa37d674560f634300155d0
AMD <michojekunle1@gmail.com>
2022-09-07 17:23:11 +0100

    config's for PWA

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ChannelCard = () => {
   return (
     <div>
-      
+      Channel
     </div>
   )
 }
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const VideoCard = () => {
   return (
     <div>
-      
+      Video
     </div>
   )
 }
