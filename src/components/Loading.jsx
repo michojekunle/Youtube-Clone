c0f35777403f17a093292f5750200f844d6d1167
AMD <michojekunle1@gmail.com>
2022-09-12 11:49:24 +0100

    made changes

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Loading = () => <div style={{width: '100%', height: '80vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#000'}}><div class="lds-ripple"><div></div><div></div></div></div> ;
+const Loading = () => <div style={{width: '100%', height: '90vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#000'}}><div class="lds-ripple"><div></div><div></div></div></div> ;
 
 export default Loading
